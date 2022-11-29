70859f683d442f8bf9002bb04bd80429babc4e7e
Giovanna Carvalho <100374203+ProjetosCarvalho@users.noreply.github.com>
2022-11-29 16:52:09 -0300

    Add files via upload

---
+++ teste_js/teste.js
@@ -0,0 +1,7 @@
+// document.write('Testando JS no HTML');
+// alert('Escreva qualquer coisa');
+
+var nome = prompt('Digite seu nome')
+
+alert('Bem vindo' + nome)
+document.write('Bem vindo ' + nome)
